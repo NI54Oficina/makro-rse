58fa8457aaacd23912f2f3db60a60345e7e7b65a
melmiranda <melania.smc@hotmail.com>
2017-02-14 11:00:00 -0300

    10:59 14-02-2017

--- rse.php
+++ rse.php
@@ -1,3 +1,122 @@
-<div class="row">
-<p>Texto rse?</p>
+<link rel="stylesheet" href="style.css">
+<div id="rse-container" class="row col-lg-12 col-md-12 col-sm-12 col-xs-12">
+
+
+  <div class="col-lg-5 col-md-5 col-sm-12 col-xs-12 left-upper-box">
+    <h1 class="color-lightblue">MAKRO <br>  <span>ARGENTINA </span></h1>
+    <h2 class="color-green"><span>POLITICA</span> <span>SUSTENTABILIDAD</span> <span>& INNOVACIÓN</span></h2>
+    <p>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.</p>
+    <p>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.</p>
+
+    <img src="recursos/foco.png" alt="">
+
+  </div>
+
+
+
+  <div class="col-lg-7 col-md-7 col-sm-12 col-xs-12 right-upper-box">
+
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 upper-box-inner inner-1">
+      <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3"><img src="recursos/img-01.jpg" alt=""></div>
+      <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3"><img src="recursos/img-02.jpg" alt=""></div>
+      <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3"><img src="recursos/img-03.jpg" alt=""></div>
+      <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3"><img src="recursos/img-04.jpg" alt=""></div>
+    </div>
+
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 upper-box-inner inner-2">
+      <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+        <h3 class="color-green"><span class="color-lightblue">NUESTRA</span> VISION</h3>
+        <p>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto </p>
+      </div>
+
+      <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+        <h3 class="color-green"><span class="color-lightblue">NUESTRA</span> MISION</h3>
+        <p>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto </p>
+      </div>
+
+    </div>
+
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 upper-box-inner inner-3">
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+          <h3 class="color-green"><span class="color-lightblue">NUESTRO</span> PRINCIPIO</h3>
+          <p>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto </p>
+        </div>
+
+    </div>
+
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 upper-box-inner inner-4">
+
+      <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+        <h3 class="color-green"><span class="color-lightblue">ASUNTOS</span> IMPORTANTES</h3>
+        <p>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto </p>
+        <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+          <li>contenido contenido</li>
+          <li>contenido contenido</li>
+          <li>contenido contenido</li>
+          <li>contenido contenido</li>
+        </div>
+
+        <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+          <li>contenido contenido</li>
+          <li>contenido contenido</li>
+          <li>contenido contenido</li>
+          <li>contenido contenido</li>
+        </div>
+
+      </div>
+
+    </div>
+
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 upper-box-inner inner-5">
+
+      <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3"><img src="recursos/compromisso.png" alt=""></div>
+      <div class="col-lg-9 col-md-9 col-sm-9 col-xs-9">
+        <h3 class="color-green"><span class="color-lightblue">NUESTRO</span> COMPROMISO</h3>
+        <p>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto Lorem Ipsum es Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto Lorem Ipsum es</p>
+
+        <li><span class="color-amarillo">Algo de color</span> contenido contenido</li>
+        <li><span class="color-turquesa">Algo de color</span> contenido contenido</li>
+        <li><span class="color-verde">Algo de color</span> contenido contenido</li>
+
+      </div>
+
+    </div>
+
+  </div>
+
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 objetivo-p">
+    <p><span>OBJETIVO</span> algo de los objetivos sara saraza saraza saraza saraza saraza</p>
+  </div>
+
+  <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 bottom-box-container">
+
+    <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4 container-title">
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 title-amarillo title">
+          <div class="col-lg-5 col-md-5 col-sm-5 col-xs-5"><img src="recursos/ico-pessoas.png" alt=""><label for="">PERSONAS</label></div>
+          <div class="col-lg-7 col-md-7 col-sm-7 col-xs-7"><p> UN TITULO SARAZA SARAZA</p></div>
+        </div>
+    </div>
+
+    <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4 container-title">
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 title-verde title">
+          <div class="col-lg-5 col-md-5 col-sm-5 col-xs-5"><img src="recursos/ico-planeta.png" alt=""><label for="">PLANETA</label></div>
+          <div class="col-lg-7 col-md-7 col-sm-7 col-xs-7"><p> UN TITULO SARAZA SARAZA</p></div>
+        </div>
+    </div>
+
+    <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4 container-title">
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 title-turquesa title">
+          <div class="col-lg-5 col-md-5 col-sm-5 col-xs-5"><img src="recursos/ico-lucro.png" alt=""><label for="">GANANCIA </label></div>
+          <div class="col-lg-7 col-md-7 col-sm-7 col-xs-7"><p> UN TITULO SARAZA SARAZA</p></div>
+        </div>
+    </div>
+
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 container-img">
+      <img src="recursos/imagen-inferior.png" alt="">
+    </div>
+
+
+  </div>
+
+
 </div>
