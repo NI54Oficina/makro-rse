8d04fc2bf77a3376a62fa23026a63516ad9bc95f
Fran <fran@ni54.com>
2017-02-20 12:23:20 -0300

    20-02 12:23
    ajustes varios

--- rse.php
+++ rse.php
@@ -1,6 +1,5 @@
 <link rel="stylesheet" href="style.css">
 
-<div id="rse-container" style="padding:0" class="row col-lg-12 col-md-12 col-sm-12 col-xs-12">
 
 <div class="container-fluid main_content">
     <div class="row">
@@ -11,13 +10,13 @@
                     <div class="arrow-up"></div>
                 </div>
             </div>
-<div id="rse-container" class="row col-lg-12 col-md-12 col-sm-12 col-xs-12">
+<div id="rse-container" style="padding:0;margin-left:0;" class="row col-lg-12 col-md-12 col-sm-12 col-xs-12" >
 
 
 
-  <div class="col-lg-5 col-md-5 col-sm-12 col-xs-12 left-upper-box">
-    <h1 class="color-lightblue">MAKRO <br>  <span>ARGENTINA </span></h1>
-    <h2 class="color-green"><span>POLITICA</span> <span>SUSTENTABILIDAD</span> <span>& INNOVACIÓN</span></h2>
+  <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 left-upper-box">
+    <h1 class="color-lightblue" style="letter-spacing:0.05em;">MAKRO <br>  <span style="letter-spacing:initial;">ARGENTINA </span></h1>
+    <h2 class="color-green"><span style="letter-spacing:0.05em;">POLITICA</span> <span>SUSTENTABILIDAD</span><br> <span>& INNOVACIÓN</span></h2>
     <p>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.</p>
     <p>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.</p>
 
@@ -27,7 +26,7 @@
 
 
 
-  <div class="col-lg-7 col-md-7 col-sm-12 col-xs-12 right-upper-box">
+  <div class="col-lg-8 col-md-8 col-sm-12 col-xs-12 right-upper-box">
 
     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 upper-box-inner inner-1">
       <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3"><img src="recursos/img-01.jpg" alt=""></div>
@@ -98,7 +97,7 @@
   </div>
 
     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 objetivo-p">
-    <p class="col-lg-2 col-md-2 col-sm-2 col-xs-2"><span>OBJETIVO</span></p> <p class="col-lg-10 col-md-10 col-sm-10 col-xs-10">algo de los objetivos sara saraza saraza saraza saraza saraza algo de los objetivos sara saraza saraza saraza saraza saraza algo de los objetivos sara saraza saraza saraza saraza saraza</p>
+    <p class="col-lg-2 col-md-2 col-sm-2 col-xs-2"><span style="line-height:initial;">OBJETIVO</span></p> <p class="col-lg-10 col-md-10 col-sm-10 col-xs-10">algo de los objetivos sara saraza saraza saraza saraza saraza algo de los objetivos sara saraza saraza saraza saraza saraza algo de los objetivos sara saraza saraza saraza saraza saraza</p>
   </div>
 
   <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 bottom-box-container">
