d67ea3326cf07cc1dbcfaf4e3e5b6df743995351
Fran <fran@ni54.com>
2017-03-13 16:56:42 -0300

    13-03 16:56

--- rse.php
+++ rse.php
@@ -10,147 +10,172 @@
                     <div class="arrow-up"></div>
                 </div>
             </div>
-<div id="rse-container" style="padding:0;margin-left:0;max-width:600px;" class="row col-lg-12 col-md-12 col-sm-12 col-xs-12"  >
+<div style="width:100%; text-align:center;">
+<div id="rse-container" style="padding:0;margin-left:0;max-width:650px;display:inline-block;float:initial;text-align:left;" class="row col-lg-12 col-md-12 col-sm-12 col-xs-12"  >
 
 
-
-  <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 left-upper-box ">
-	<div class="left-upper-upper top-dashed bottom-dashed" style="padding-top:15px;padding-bottom:0px;">
-    <span class="right-dashed upper-text " >
-	<h1 class="color-lightblue" style="letter-spacing:0.05em;">MAKRO <br>  <span style="letter-spacing:initial;">ARGENTINA </span></h1>
-	</span>
-	 <span class="upper-text">
-    <h2 class="color-green" style="line-height:0.8em;"><span style="letter-spacing:0.05em;font-size:1.4em">POLITICA</span> <br><span style="font-size:0.8em;">SUSTENTABILIDAD</span><br> <span>& INNOVACIÓN</span></h2>
-	</span>
+	<hr class="dashed" />
+	<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 left-upper-box ">
+		<div class="left-upper-upper" style="padding-top:15px;padding-bottom:0px;">
+			<img src="assets/images/hojita.png" style="display:inline-block;float:left;"/>
+			<span class="right-dashed upper-text " >
+			<h1 class="color-lightblue" style="letter-spacing:0.1em;">MAKRO<br><span style="letter-spacing:initial;">ARGENTINA </span></h1>
+			</span>
+			 <span class="upper-text">
+			<h2 class="color-green" style="line-height:0.8em;"><span style="letter-spacing:0.05em;font-size:1.4em">POLITICA</span> <br><span style="font-size:0.8em;">SUSTENTABILIDAD</span><br> <span>& INNOVACIÓN</span></h2>
+			</span>
+			<img src="assets/images/hojita.png" style="display:inline-block;float:right;" />
+		</div>
 	</div>
-    <p>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.</p>
-    <p>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.</p>
+	<hr class="dashed" />
+	<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 left-upper-box ">
+		<p>Makro es parte de SHV Holdings, una multinacional familiar activa en una variedad de operaciones y selectas actividades de inversión. Con base en Holanda, SHV está presente en más de 60 países en todos los continentes, brindando empleo a aproximadamente 60,000 personas. La naturaleza de sus negocios conecta a la compañía con las sociedades donde opera. Por más de 120 años, la estrategia de SHV se construye sobre su ADN único y su filosofía corporativa, que es invertir en las personas, enfocado en el crecimiento continuo  y el compromiso de crear una cultura donde la innovación lidera la creación de valor sustentable a largo plazo.</p><br>
 
-    <!--<img src="recursos/foco.png" alt="">!-->
+		<p>En Makro, compartimos esta filosofía y creemos firmemente que hacer negocios de manera sustentable se trata de hacer las cosas mejor, creando la diferencia. Es por eso que innovación es la clave para nuestro éxito en el futuro. Nuestra ambición es desarrollar procesos, procedimientos y productos nuevos e innovadores que concretarán beneficios para nuestro negocio, comunidad y medio ambiente. Nuestra posición estratégica en la cadena de valor nos permite hacer eso perfectamente, al influenciar las diferentes partes interesadas y, como resultado, generar impactos positivos de diferentes formas . </p>
 
-  </div>
-  
+		<!--<img src="recursos/foco.png" alt="">!-->
+	</div>
+	
+	
+	<hr class="dashed" />
 
-
-
-  <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 right-upper-box top-dashed">
-
-    <!--<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 upper-box-inner inner-1">
-      <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3"><img src="recursos/img-01.jpg" alt=""></div>
-      <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3"><img src="recursos/img-02.jpg" alt=""></div>
-      <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3"><img src="recursos/img-03.jpg" alt=""></div>
-      <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3"><img src="recursos/img-04.jpg" alt=""></div>
-    </div>!-->
 
     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 upper-box-inner inner-2">
       <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
         <h3 class="color-green"><span class="color-lightblue">NUESTRA</span> VISION</h3>
-        <p>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto </p>
+        <p>Responsabilidad y compromiso con el futuro.
+Makro será reconocido por sus colaboradores, proveedores, clientes y socios como una compañía responsable del impacto generado por su operación y comprometida con un futuro mejor para las actuales y próximas generaciones.</p>
       </div>
 
       <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
         <h3 class="color-green"><span class="color-lightblue">NUESTRA</span> MISION</h3>
-        <p>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto </p>
+        <p>Buscar nuevas oportunidades para desarrollar nuestro negocio y generar influencia positiva en la cadena de valor, a través del uso eficiente de los recursos disponibles, con el fin de reducir el impacto que nos rodea y contribuir a la estabilidad del planeta, ahora y en el futuro.</p>
       </div>
 
     </div>
-
-    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 upper-box-inner inner-3 top-dashed">
+	<hr class="dashed" />
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 upper-box-inner inner-3">
         <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
           <h3 class="color-green"><span class="color-lightblue">NUESTRO</span> PRINCIPIO</h3>
-          <p>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto </p>
+          <p>La búsqueda de altos niveles de sustentabilidad en nuestro negocio, dará paso a la innovación: productos innovadores, procesos, infraestructura, desarrollo de personas y servicio al cliente. Cualquier idea nueva que busque cambiar la forma en que hacemos negocios, debe considerar el impacto en las personas, el planeta y los resultados.</p>
         </div>
 
     </div>
-
-    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 upper-box-inner inner-4 top-dashed">
+	<hr class="dashed" />
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 upper-box-inner inner-4">
 
       <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
         <h3 class="color-green"><span class="color-lightblue">ASUNTOS</span> IMPORTANTES</h3>
-        <p>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto </p>
+        <p>Nuestra empresa se enfrenta a muchos desafíos y oportunidades. En base a nuestro análisis de materialidad y las directrices de SHV Holdings, hemos dado prioridad a los siguientes asuntos:</p>
         <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-          <li>contenido contenido</li>
-          <li>contenido contenido</li>
-          <li>contenido contenido</li>
-          <li>contenido contenido</li>
+          <li>• Cumplimiento legal ambiental;</li>
+          <li>• Aumento de rendimiento económico;</li>
+          <li>• Desarrollo de colaboradores;</li>
+          <li>• Satisfacción y desarrollo de clientes;</li>
         </div>
 
         <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-          <li>contenido contenido</li>
-          <li>contenido contenido</li>
-          <li>contenido contenido</li>
-          <li>contenido contenido</li>
+          <li>• Salud, seguridad y bienestar de nuestros grupos
+   de interés internos y externos;</li>
+          <li>• Ética en la actividad diaria;</li>
+          <li>• Innovación sustentable;</li>
+          <li>• Comunicación abierta y reporte.</li>
         </div>
 
       </div>
 
     </div>
-
-    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 upper-box-inner inner-5 top-dashed">
+	<hr class="dashed" />
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 upper-box-inner inner-5">
 
      
       <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
         <h3 class="color-green"><span class="color-lightblue">NUESTRO</span> COMPROMISO</h3>
-        <p>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto Lorem Ipsum es Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto Lorem Ipsum es</p>
+        <p>Buscaremos continuamente nuevas oportunidades para asegurar que estamos generando un impacto positivo en los tres pilares de la sustentabilidad y la innovación: </p>
 
        
 
       </div>
-		 <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4"><img src="recursos/compromisso.png" alt=""></div>
-		 <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4"> <li><span class="color-amarillo">Algo de color</span> contenido contenido</li>
-        <li><span class="color-turquesa">Algo de color</span> contenido contenido</li>
-        <li><span class="color-verde">Algo de color</span> contenido contenido</li></div>
+		 <div class="col-lg-5 col-md-5 col-sm-5 col-xs-5"><img src="assets/images/circulos-copromiso.png" alt=""></div>
+		 <div class="col-lg-7 col-md-7 col-sm-7 col-xs-7 lateral-compromiso">
+		 <div class="col-lg-4"><img src="assets/images/personas.png" style="display:inline-block;" /><span class="color-amarillo">Personas</span><br>
+		 Ayudar a las personas a ser el cambio.</div>
+        <div class="col-lg-4"><img src="assets/images/dinero.png" style="display:inline-block;" /><span class="color-turquesa">Planeta</span><br>
+		Hacer más con menos.</div>
+        <div class="col-lg-4"><img src="assets/images/planeta.png" style="display:inline-block;" /><span class="color-verde">Ganancia</span><br>
+		Crear relaciones de beneficio mutuo.</div>
+		</div>
     </div>
+	<hr class="dashed" />
+	
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+        <h3 class="color-green"><span class="color-lightblue">NUESTROS</span> OBJETIVOS</h3>
+        <p>Hemos establecido objetivos a mediano plazo hasta el 2018 para hacer tangible nuestro compromiso  y tomar acción en relación a los tres pilares de sustentabilidad e innovación:</p>
+	</div>
 
-  </div>
+	<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 bottom-box-container">
 
-    <!--<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 objetivo-p">
-    <p class="col-lg-2 col-md-2 col-sm-2 col-xs-2"><span style="line-height:initial;">OBJETIVO</span></p> <p class="col-lg-10 col-md-10 col-sm-10 col-xs-10">algo de los objetivos sara saraza saraza saraza saraza saraza algo de los objetivos sara saraza saraza saraza saraza saraza algo de los objetivos sara saraza saraza saraza saraza saraza</p>
-  </div>!-->
-  
-    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 top-dashed">
-        <h3 class="color-green"><span class="color-lightblue">NUESTRO</span> COMPROMISO</h3>
-        <p>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto Lorem Ipsum es Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto Lorem Ipsum es</p>
+		<div class="col-lg-4 col-md-4 col-sm-4 col-xs-4 container-title">
+			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 title-amarillo title">
+			  <div class="col-lg-5 col-md-5 col-sm-5 col-xs-5"><img src="recursos/ico-pessoas.png" alt=""><label for="">PERSONAS</label></div>
+			  <div class="col-lg-7 col-md-7 col-sm-7 col-xs-7"><p>PERMITIR A LAS PERSONAS SER EL CAMBIO</p></div>
+			</div>
+		</div>
 
-       
+		<div class="col-lg-4 col-md-4 col-sm-4 col-xs-4 container-title">
+			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 title-verde title">
+			  <div class="col-lg-5 col-md-5 col-sm-5 col-xs-5"><img src="recursos/ico-planeta.png" alt=""><label for="">PLANETA</label></div>
+			  <div class="col-lg-7 col-md-7 col-sm-7 col-xs-7"><p>HACER MÁS CON MENOS</p></div>
+			</div>
+		</div>
 
-      </div>
+		<div class="col-lg-4 col-md-4 col-sm-4 col-xs-4 container-title">
+			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 title-turquesa title">
+			  <div class="col-lg-5 col-md-5 col-sm-5 col-xs-5"><img src="recursos/ico-lucro.png" alt=""><label for="">GANANCIA </label></div>
+			  <div class="col-lg-7 col-md-7 col-sm-7 col-xs-7"><p>CREAR RELACIONES DE BENEFICIO MUTUO</p></div>
+			</div>
+		</div>
 
-  <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 bottom-box-container">
-
-    <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4 container-title">
-        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 title-amarillo title">
-          <div class="col-lg-5 col-md-5 col-sm-5 col-xs-5"><img src="recursos/ico-pessoas.png" alt=""><label for="">PERSONAS</label></div>
-          <div class="col-lg-7 col-md-7 col-sm-7 col-xs-7"><p> UN TITULO SARAZA SARAZA</p></div>
-        </div>
-    </div>
-
-    <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4 container-title">
-        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 title-verde title">
-          <div class="col-lg-5 col-md-5 col-sm-5 col-xs-5"><img src="recursos/ico-planeta.png" alt=""><label for="">PLANETA</label></div>
-          <div class="col-lg-7 col-md-7 col-sm-7 col-xs-7"><p> UN TITULO SARAZA SARAZA</p></div>
-        </div>
-    </div>
-
-    <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4 container-title">
-        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 title-turquesa title">
-          <div class="col-lg-5 col-md-5 col-sm-5 col-xs-5"><img src="recursos/ico-lucro.png" alt=""><label for="">GANANCIA </label></div>
-          <div class="col-lg-7 col-md-7 col-sm-7 col-xs-7"><p> UN TITULO SARAZA SARAZA</p></div>
-        </div>
-    </div>
-
-    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 container-img">
-      <img src="recursos/imagen-inferior.png" alt="">
-    </div>
+		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 container-img">
+		  <img src="assets/images/objetivos.png" alt="">
+		</div>
 
 
-  </div>
-
-
+	</div>
+	<hr class="dashed" />
+	<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+		<h3 class="color-green"><span class="color-lightblue">PROGRAMAS SOCIALES</span> QUE APOYAMOS</h3>
+		<div class="programas-containers"><div class="logo-container "><img src="assets/images/logo-pescar.png" /></div><p>Es una organización civil sin fines de lucro cuyo objetivo principal es promover y ejecutar acciones orientadas al fortalecimiento del vínculo entre educación, juventud y empleo. Para lograrlo, la Fundación posee diversas líneas de acción entre las cuales se destaca el Programa “Centros Pescar – Educando para el Trabajo”. El mismo consiste en la implementación de centros educativos que brindan formación personal y laboral a jóvenes de escasas oportunidades.<br>
+		Dentro de Makro funcionan 3 centros, en las tiendas de Córdoba, San Martín y Quilmes, donde participa un promedio de 15 chicos por centro todos los años.</p></div>
+		<div class="programas-containers"><div class="logo-container "><img src="assets/images/logo-techo.png" style="padding:0;" /></div><p>Es una organización tiene 15 años de historia en 19 países de Sudamérica y Centro América. Los planes contribuyen en la construcción de viviendas para personas de bajos recursos, junto con la organización de mesas vecinales, programas de capacitación, trabajo, salud y educación. Makro se sumó a esta causa comunitaria desde 2013. En promedio, construimos seis casas por año y donamos otras seis.</p></div>
+		<div class="programas-containers"><div class="logo-container "><img src="assets/images/logo-united.png" /></div><p>Es una Organización de la Sociedad Civil Argentina que desde el 2008 se dedica al desarrollo de proyectos de promoción social de sectores vulnerables de la Argentina a través de la obtención y canalización de recursos. Los ejes del trabajo son: inversión social, voluntariado corporativo e individual, donaciones en especie, para los proyectos y catástrofes y proyectos corporativos.<br>
+	Makro participa del programa “Invertir Vale la Pena” desde 2014 en conjunto con P&G. En éste, doce voluntarios de cada una de las empresas dictan doce Módulos relacionados a Finanzas e Inserción Laboral, entre otros.</p></div>
+		<div class="programas-containers"><div class="logo-container "><img src="assets/images/che-logo.png" /></div><p>Es una fundación que desde el año 2007 se empeña por mejorar la educación y fomentar los emprendedores social en Argentina. Makro, a través de SHV, da apoyo económico a esta Fundación en donde se capacitan jóvenes que aprenden a montar un proyecto. Muchos de los jóvenes con quienes trabajan se encuentran en situaciones de vulnerabilidad.<br>
+	Desde el año 2013, a través de Che Amigo Makro articula con la Asociación Civil 25 de Mayo, que realiza actividades culturales y deportivas y capacitando jóvenes en diferentes aspectos. Se basan en la promoción del fútbol como una actividad de recreación e intercambio, que además de estimular el desarrollo físico y mental, utiliza el juego para aprender a valorar el trabajo en equipo.</p></div>
+	</div>
 </div>
 
+  </div>
   </div>
     </div>
 
 
 	</div>
+<script>
+
+function CenterToParent(){
+	console.log("center");
+$(".").each(function(){
+		$(this).css("margin-top",0);
+		$(this).css("margin-bottom",0);
+		$(this).css("padding-bottom",0);
+		$(this).css("padding-top",0);
+		var parent= $(this).parent();
+		//console.log($(this).outerHeight());
+		if($(this).outerHeight()>0){
+			var paddingTop= ($(parent).innerHeight()/2)-($(this).outerHeight()/2);
+			$(this).css("padding-top",paddingTop+"px");
+		}
+	});
+}
+</script>
