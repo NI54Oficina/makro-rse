1419f2510034554fc7cfbf8d4806a134f449fcc9
Fran <fran@ni54.com>
2017-03-15 10:42:31 -0300

    15-03 10:42

--- rse.php
+++ rse.php
@@ -1,4 +1,4 @@
-<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="assets/css/rse.css">
 
 
 <div class="container-fluid main_content">
@@ -11,28 +11,29 @@
                 </div>
             </div>
 <div style="width:100%; text-align:center;">
-<div id="rse-container" style="padding:0;margin-left:0;max-width:650px;display:inline-block;float:initial;text-align:left;" class="row col-lg-12 col-md-12 col-sm-12 col-xs-12"  >
+<div id="rse-container" style="padding:0;margin-left:0;max-width:850px;display:inline-block;float:initial;text-align:left;" class="row col-lg-12 col-md-12 col-sm-12 col-xs-12"  >
 
 
 	<hr class="dashed" />
 	<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 left-upper-box ">
 		<div class="left-upper-upper" style="padding-top:15px;padding-bottom:0px;">
-			<img src="assets/images/hojita.png" style="display:inline-block;float:left;"/>
+			<img src="assets/images/hojita.png" style="display:inline-block;float:left;padding-left:30px;"/>
 			<span class="right-dashed upper-text " >
-			<h1 class="color-lightblue" style="letter-spacing:0.1em;">MAKRO<br><span style="letter-spacing:initial;">ARGENTINA </span></h1>
+			<h1 class="color-lightblue" style="">MAKRO<br><span style="letter-spacing:initial;">ARGENTINA </span></h1>
 			</span>
 			 <span class="upper-text">
-			<h2 class="color-green" style="line-height:0.8em;"><span style="letter-spacing:0.05em;font-size:1.4em">POLITICA</span> <br><span style="font-size:0.8em;">SUSTENTABILIDAD</span><br> <span>& INNOVACIÓN</span></h2>
+			<h2 class="color-green" style="line-height:1em;"><span style="font-size:1.6em">POLITÍCA</span><br><span style="font-size:0.8em;">SUSTENTABILIDAD</span><br> <span>& INNOVACIÓN</span></h2>
 			</span>
-			<img src="assets/images/hojita.png" style="display:inline-block;float:right;" />
+			<img src="assets/images/hojita.png" style="display:inline-block;float:right;padding-right:30px;" />
 		</div>
 	</div>
 	<hr class="dashed" />
-	<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 left-upper-box ">
-		<p>Makro es parte de SHV Holdings, una multinacional familiar activa en una variedad de operaciones y selectas actividades de inversión. Con base en Holanda, SHV está presente en más de 60 países en todos los continentes, brindando empleo a aproximadamente 60,000 personas. La naturaleza de sus negocios conecta a la compañía con las sociedades donde opera. Por más de 120 años, la estrategia de SHV se construye sobre su ADN único y su filosofía corporativa, que es invertir en las personas, enfocado en el crecimiento continuo  y el compromiso de crear una cultura donde la innovación lidera la creación de valor sustentable a largo plazo.</p><br>
+	<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 upper-box-inner">
+		<div>
+		<p>Makro es parte de <strong>SHV Holdings</strong>, una <strong>multinacional familiar</strong> activa en una variedad de operaciones y selectas actividades de inversión. Con base en Holanda, SHV está presente en más de 60 países en todos los continentes, brindando empleo a aproximadamente <strong>60,000 personas</strong>. La naturaleza de sus negocios conecta a la compañía con las sociedades donde opera. Por más de <strong>120 años</strong>, la estrategia de SHV se construye sobre su ADN único y su filosofía corporativa, que es <strong>invertir en las personas</strong>, enfocado en el <strong>crecimiento continuo</strong> y el compromiso de crear una cultura donde la <strong>innovación</strong> lidera la creación de valor <strong>sustentable a largo plazo.</strong></p><br>
 
-		<p>En Makro, compartimos esta filosofía y creemos firmemente que hacer negocios de manera sustentable se trata de hacer las cosas mejor, creando la diferencia. Es por eso que innovación es la clave para nuestro éxito en el futuro. Nuestra ambición es desarrollar procesos, procedimientos y productos nuevos e innovadores que concretarán beneficios para nuestro negocio, comunidad y medio ambiente. Nuestra posición estratégica en la cadena de valor nos permite hacer eso perfectamente, al influenciar las diferentes partes interesadas y, como resultado, generar impactos positivos de diferentes formas . </p>
-
+		<p>En Makro, compartimos esta filosofía y creemos firmemente que <strong>hacer negocios de manera sustentable se trata de hacer las cosas mejor, creando la diferencia</strong>. Es por eso que innovación es la clave para nuestro éxito en el futuro. Nuestra ambición es desarrollar procesos, procedimientos y productos nuevos e innovadores que concretarán beneficios para nuestro negocio, comunidad y medio ambiente. Nuestra posición <strong>estratégica en la cadena de valor</strong> nos permite hacer eso perfectamente, al influenciar las diferentes partes interesadas y, como resultado, generar impactos positivos de diferentes formas . </p>
+		</div>
 		<!--<img src="recursos/foco.png" alt="">!-->
 	</div>
 	
@@ -41,15 +42,15 @@
 
 
     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 upper-box-inner inner-2">
-      <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-        <h3 class="color-green"><span class="color-lightblue">NUESTRA</span> VISION</h3>
-        <p>Responsabilidad y compromiso con el futuro.
+      <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12" style="padding-right:15px;">
+        <h3 class="color-green"><span class="color-lightblue">NUESTRA</span> VISIÓN</h3>
+        <p><strong>Responsabilidad</strong> y compromiso con el futuro.
 Makro será reconocido por sus colaboradores, proveedores, clientes y socios como una compañía responsable del impacto generado por su operación y comprometida con un futuro mejor para las actuales y próximas generaciones.</p>
       </div>
 
-      <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-        <h3 class="color-green"><span class="color-lightblue">NUESTRA</span> MISION</h3>
-        <p>Buscar nuevas oportunidades para desarrollar nuestro negocio y generar influencia positiva en la cadena de valor, a través del uso eficiente de los recursos disponibles, con el fin de reducir el impacto que nos rodea y contribuir a la estabilidad del planeta, ahora y en el futuro.</p>
+      <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12" style="padding-left:15px;">
+        <h3 class="color-green"><span class="color-lightblue">NUESTRA</span> MISIÓN</h3>
+        <p><strong>Buscar nuevas oportunidades para desarrollar nuestro negocio y generar influencia positiva en la cadena de valor</strong>, a través del uso eficiente de los recursos disponibles, con el fin de reducir el impacto que nos rodea y contribuir a la estabilidad del planeta, ahora y en el futuro.</p>
       </div>
 
     </div>
@@ -57,7 +58,7 @@ Makro será reconocido por sus colaboradores, proveedores, clientes y socios com
     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 upper-box-inner inner-3">
         <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
           <h3 class="color-green"><span class="color-lightblue">NUESTRO</span> PRINCIPIO</h3>
-          <p>La búsqueda de altos niveles de sustentabilidad en nuestro negocio, dará paso a la innovación: productos innovadores, procesos, infraestructura, desarrollo de personas y servicio al cliente. Cualquier idea nueva que busque cambiar la forma en que hacemos negocios, debe considerar el impacto en las personas, el planeta y los resultados.</p>
+          <p>La búsqueda de altos niveles de <strong>sustentabilidad en nuestro negocio, dará paso a la innovación</strong>: productos innovadores, procesos, infraestructura, desarrollo de personas y servicio al cliente. Cualquier idea nueva que busque cambiar la forma en que hacemos negocios, debe considerar el impacto en las personas, el planeta y los resultados.</p>
         </div>
 
     </div>
@@ -66,20 +67,20 @@ Makro será reconocido por sus colaboradores, proveedores, clientes y socios com
 
       <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
         <h3 class="color-green"><span class="color-lightblue">ASUNTOS</span> IMPORTANTES</h3>
-        <p>Nuestra empresa se enfrenta a muchos desafíos y oportunidades. En base a nuestro análisis de materialidad y las directrices de SHV Holdings, hemos dado prioridad a los siguientes asuntos:</p>
+        <p>Nuestra empresa se enfrenta a muchos desafíos y oportunidades. En base a nuestro análisis de materialidad y las directrices de SHV Holdings, hemos dado prioridad a los siguientes asuntos:</p><br>
         <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-          <li>• Cumplimiento legal ambiental;</li>
-          <li>• Aumento de rendimiento económico;</li>
-          <li>• Desarrollo de colaboradores;</li>
-          <li>• Satisfacción y desarrollo de clientes;</li>
+          <li><span><strong>Cumplimiento legal</strong> ambiental;</span></li>
+          <li><span>Aumento de <strong>rendimiento económico</strong>;</span></li>
+          <li><span>Desarrollo de <strong>colaboradores</strong>;</span></li>
+          <li><span>Satisfacción y desarrollo de <strong>clientes</strong>;</span></li>
         </div>
 
         <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-          <li>• Salud, seguridad y bienestar de nuestros grupos
-   de interés internos y externos;</li>
-          <li>• Ética en la actividad diaria;</li>
-          <li>• Innovación sustentable;</li>
-          <li>• Comunicación abierta y reporte.</li>
+          <li><span><strong>Salud, seguridad y bienestar</strong> de nuestros grupos
+   de interés internos y externos;</span></li>
+          <li><span><strong>Ética</strong> en la actividad diaria;</span></li>
+          <li><span><strong>Innovación sustentable</strong>;</span></li>
+          <li><span><strong>Comunicación</strong> abierta y reporte.</span></li>
         </div>
 
       </div>
@@ -98,61 +99,62 @@ Makro será reconocido por sus colaboradores, proveedores, clientes y socios com
       </div>
 		 <div class="col-lg-5 col-md-5 col-sm-5 col-xs-5"><img src="assets/images/circulos-copromiso.png" alt=""></div>
 		 <div class="col-lg-7 col-md-7 col-sm-7 col-xs-7 lateral-compromiso">
-		 <div class="col-lg-4"><img src="assets/images/personas.png" style="display:inline-block;" /><span class="color-amarillo">Personas</span><br>
-		 Ayudar a las personas a ser el cambio.</div>
-        <div class="col-lg-4"><img src="assets/images/dinero.png" style="display:inline-block;" /><span class="color-turquesa">Planeta</span><br>
-		Hacer más con menos.</div>
-        <div class="col-lg-4"><img src="assets/images/planeta.png" style="display:inline-block;" /><span class="color-verde">Ganancia</span><br>
-		Crear relaciones de beneficio mutuo.</div>
+		 <div class="col-lg-4"><img src="assets/images/personas.png" style="display:inline-block;" /><br><strong><span class="color-amarillo">Personas</span></strong><br>
+		 <p>Ayudar a las personas a ser el cambio.</p></div>
+        <div class="col-lg-4"><img src="assets/images/dinero.png" style="display:inline-block;" /><br><strong><span class="color-turquesa">Planeta</span></strong><br>
+		<p>Hacer más con menos.</p></div>
+        <div class="col-lg-4"><img src="assets/images/planeta.png" style="display:inline-block;" /><br><strong><span class="color-verde">Ganancia</span></strong><br>
+		<p>Crear relaciones de beneficio mutuo.</p></div>
 		</div>
     </div>
 	<hr class="dashed" />
 	
-    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 upper-box-inner">
         <h3 class="color-green"><span class="color-lightblue">NUESTROS</span> OBJETIVOS</h3>
-        <p>Hemos establecido objetivos a mediano plazo hasta el 2018 para hacer tangible nuestro compromiso  y tomar acción en relación a los tres pilares de sustentabilidad e innovación:</p>
+        <p >Hemos establecido objetivos a mediano plazo hasta el 2018 para hacer tangible nuestro compromiso  y tomar acción en relación a los tres pilares de sustentabilidad e innovación:</p>
 	</div>
 
 	<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 bottom-box-container">
-
+		<br>
 		<div class="col-lg-4 col-md-4 col-sm-4 col-xs-4 container-title">
 			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 title-amarillo title">
-			  <div class="col-lg-5 col-md-5 col-sm-5 col-xs-5"><img src="recursos/ico-pessoas.png" alt=""><label for="">PERSONAS</label></div>
-			  <div class="col-lg-7 col-md-7 col-sm-7 col-xs-7"><p>PERMITIR A LAS PERSONAS SER EL CAMBIO</p></div>
+			  <div class="col-lg-5 col-md-5 col-sm-5 col-xs-5"><img src="assets/images/personas-down.png" alt=""><label for="" class="sans-bold">PERSONAS</label></div>
+			  <div class="col-lg-7 col-md-7 col-sm-7 col-xs-7"><p class="sans-bold">PERMITIR A LAS PERSONAS SER EL CAMBIO</p></div>
 			</div>
 		</div>
 
 		<div class="col-lg-4 col-md-4 col-sm-4 col-xs-4 container-title">
 			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 title-verde title">
-			  <div class="col-lg-5 col-md-5 col-sm-5 col-xs-5"><img src="recursos/ico-planeta.png" alt=""><label for="">PLANETA</label></div>
-			  <div class="col-lg-7 col-md-7 col-sm-7 col-xs-7"><p>HACER MÁS CON MENOS</p></div>
+			  <div class="col-lg-5 col-md-5 col-sm-5 col-xs-5"><img src="assets/images/planeta-down.png" alt=""><label for="" class="sans-bold">PLANETA</label></div>
+			  <div class="col-lg-7 col-md-7 col-sm-7 col-xs-7"><p class="sans-bold">HACER MÁS CON MENOS</p></div>
 			</div>
 		</div>
 
 		<div class="col-lg-4 col-md-4 col-sm-4 col-xs-4 container-title">
 			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 title-turquesa title">
-			  <div class="col-lg-5 col-md-5 col-sm-5 col-xs-5"><img src="recursos/ico-lucro.png" alt=""><label for="">GANANCIA </label></div>
-			  <div class="col-lg-7 col-md-7 col-sm-7 col-xs-7"><p>CREAR RELACIONES DE BENEFICIO MUTUO</p></div>
+			  <div class="col-lg-5 col-md-5 col-sm-5 col-xs-5"><img src="assets/images/dinero-down.png" alt=""><label for="" class="sans-bold">GANANCIA </label></div>
+			  <div class="col-lg-7 col-md-7 col-sm-7 col-xs-7"><p class="sans-bold">CREAR RELACIONES DE BENEFICIO MUTUO</p></div>
 			</div>
 		</div>
-
 		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 container-img">
-		  <img src="assets/images/objetivos.png" alt="">
+		
+		  <img src="assets/images/objetivos.png" style=""alt="">
 		</div>
 
 
 	</div>
 	<hr class="dashed" />
-	<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+	<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 upper-box-inner">
 		<h3 class="color-green"><span class="color-lightblue">PROGRAMAS SOCIALES</span> QUE APOYAMOS</h3>
 		<div class="programas-containers"><div class="logo-container "><img src="assets/images/logo-pescar.png" /></div><p>Es una organización civil sin fines de lucro cuyo objetivo principal es promover y ejecutar acciones orientadas al fortalecimiento del vínculo entre educación, juventud y empleo. Para lograrlo, la Fundación posee diversas líneas de acción entre las cuales se destaca el Programa “Centros Pescar – Educando para el Trabajo”. El mismo consiste en la implementación de centros educativos que brindan formación personal y laboral a jóvenes de escasas oportunidades.<br>
 		Dentro de Makro funcionan 3 centros, en las tiendas de Córdoba, San Martín y Quilmes, donde participa un promedio de 15 chicos por centro todos los años.</p></div>
 		<div class="programas-containers"><div class="logo-container "><img src="assets/images/logo-techo.png" style="padding:0;" /></div><p>Es una organización tiene 15 años de historia en 19 países de Sudamérica y Centro América. Los planes contribuyen en la construcción de viviendas para personas de bajos recursos, junto con la organización de mesas vecinales, programas de capacitación, trabajo, salud y educación. Makro se sumó a esta causa comunitaria desde 2013. En promedio, construimos seis casas por año y donamos otras seis.</p></div>
 		<div class="programas-containers"><div class="logo-container "><img src="assets/images/logo-united.png" /></div><p>Es una Organización de la Sociedad Civil Argentina que desde el 2008 se dedica al desarrollo de proyectos de promoción social de sectores vulnerables de la Argentina a través de la obtención y canalización de recursos. Los ejes del trabajo son: inversión social, voluntariado corporativo e individual, donaciones en especie, para los proyectos y catástrofes y proyectos corporativos.<br>
 	Makro participa del programa “Invertir Vale la Pena” desde 2014 en conjunto con P&G. En éste, doce voluntarios de cada una de las empresas dictan doce Módulos relacionados a Finanzas e Inserción Laboral, entre otros.</p></div>
-		<div class="programas-containers"><div class="logo-container "><img src="assets/images/che-logo.png" /></div><p>Es una fundación que desde el año 2007 se empeña por mejorar la educación y fomentar los emprendedores social en Argentina. Makro, a través de SHV, da apoyo económico a esta Fundación en donde se capacitan jóvenes que aprenden a montar un proyecto. Muchos de los jóvenes con quienes trabajan se encuentran en situaciones de vulnerabilidad.<br>
+		<div class="programas-containers"><div class="logo-container "><img src="assets/images/che-logo.png" style="padding-bottom:40px;" /></div><p>Es una fundación que desde el año 2007 se empeña por mejorar la educación y fomentar los emprendedores social en Argentina. Makro, a través de SHV, da apoyo económico a esta Fundación en donde se capacitan jóvenes que aprenden a montar un proyecto. Muchos de los jóvenes con quienes trabajan se encuentran en situaciones de vulnerabilidad.<br>
 	Desde el año 2013, a través de Che Amigo Makro articula con la Asociación Civil 25 de Mayo, que realiza actividades culturales y deportivas y capacitando jóvenes en diferentes aspectos. Se basan en la promoción del fútbol como una actividad de recreación e intercambio, que además de estimular el desarrollo físico y mental, utiliza el juego para aprender a valorar el trabajo en equipo.</p></div>
 	</div>
+	<hr class="dashed" />
 </div>
 
   </div>
@@ -161,21 +163,3 @@ Makro será reconocido por sus colaboradores, proveedores, clientes y socios com
 
 
 	</div>
-<script>
-
-function CenterToParent(){
-	console.log("center");
-$(".").each(function(){
-		$(this).css("margin-top",0);
-		$(this).css("margin-bottom",0);
-		$(this).css("padding-bottom",0);
-		$(this).css("padding-top",0);
-		var parent= $(this).parent();
-		//console.log($(this).outerHeight());
-		if($(this).outerHeight()>0){
-			var paddingTop= ($(parent).innerHeight()/2)-($(this).outerHeight()/2);
-			$(this).css("padding-top",paddingTop+"px");
-		}
-	});
-}
-</script>
